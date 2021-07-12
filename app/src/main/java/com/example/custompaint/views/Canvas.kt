f226a8cc724e7a1ed6e87177b16ddea8c68de91d
jaroslav <drozdovji@yandex.ru>
2021-07-12 06:41:15 +0300

    adding points between touchable point

--- app/src/main/java/com/example/custompaint/views/Canvas.kt
+++ app/src/main/java/com/example/custompaint/views/Canvas.kt
@@ -1,49 +1,65 @@
 package com.example.custompaint.views
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
-import android.graphics.drawable.Drawable
+import android.graphics.Point
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.example.custompaint.R
+import kotlin.math.sqrt
 
 
 class Canvas(context: Context, attr: AttributeSet) : View(context, attr) {
 
-    private val points: ArrayList<Drawable> = arrayListOf()
+    private var points: ArrayList<Point> = arrayListOf()
+    private val pointDrawable = ContextCompat.getDrawable(context, R.drawable.point)!!
+    private var preView: Bitmap? = null
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event == null) return false
-//        if (points.isEmpty()) {
-//            val point = ContextCompat.getDrawable(context, R.drawable.point)!!
-//            point.setBounds(event.x.toInt() - 5, event.y.toInt() - 5, event.x.toInt() + 5, event.y.toInt() + 5)
-//            points.add(point)
-//            return true
-//        }
-//        val lastPoint = points.last()
-//        val deltaX = event.x.toInt() - lastPoint.bounds.left + 5
-//        val deltaY = event.y.toInt() - lastPoint.bounds.top + 5
-//        points += (1..10).map {
-//            ContextCompat.getDrawable(context, R.drawable.point)!!.apply {
-//                setBounds(
-//                    event.x.toInt() - (deltaX - deltaX/10*it) - 5,
-//                    event.y.toInt() - (deltaY - deltaY/10*it) - 5,
-//                    event.x.toInt() - (deltaX - deltaX/10*it) + 5,
-//                    event.y.toInt() - (deltaY - deltaY/10*it) + 5)
-//            }
-//        }
-        val point = ContextCompat.getDrawable(context, R.drawable.point)!!
-        point.setBounds(event.x.toInt() - 5, event.y.toInt() - 5, event.x.toInt() + 5, event.y.toInt() + 5)
-        points.add(point)
+        if (event.actionMasked == MotionEvent.ACTION_DOWN || points.isEmpty()) {
+            points.clear()
+            points.add(Point(event.x.toInt(), event.y.toInt()))
+            return true
+        }
+        val lastPoint = points.last()
+        val deltaX = event.x.toInt() - lastPoint.x
+        val deltaY = event.y.toInt() - lastPoint.y
+        val distance = sqrt((deltaX*deltaX + deltaY*deltaY).toFloat())
+        val numberPoints = (distance/5).toInt()
+        points = (1..numberPoints).map {
+            Point(event.x.toInt() - (deltaX - deltaX/numberPoints*it),
+                event.y.toInt() - (deltaY - deltaY/numberPoints*it))
+        } as ArrayList<Point>
+        preView = loadBitmapFromView()
         invalidate()
         return true
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        points.forEach { it.draw(canvas!!) }
+        if (canvas == null) return
+        if (preView != null) canvas.drawBitmap(preView!!, 0f, 0f, null)
+        points.forEach {
+            pointDrawable.setBounds(it.x - 5, it.y - 5, it.x + 5, it.y + 5)
+            pointDrawable.draw(canvas)
+        }
+    }
+
+    fun loadBitmapFromView(): Bitmap {
+        measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        val b = Bitmap.createBitmap(
+            width,
+            height,
+            Bitmap.Config.ARGB_8888
+        )
+        val c = Canvas(b)
+        layout(left, top, right, bottom)
+        draw(c)
+        return b
     }
 
 }
